620cd3771f0b1d313485cb8608a85a853d398103
dutt23 <shatyaki_dutt@hotmail.com>
2022-09-08 17:25:06 +0530

    Changes to read config from file

--- main.go
+++ main.go
@@ -4,19 +4,26 @@ import (
 	"database/sql"
 	"github/dutt23/bank/api"
 	db "github/dutt23/bank/db/sqlc"
+	"github/dutt23/bank/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5430/bank?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5430/bank?sslmode=disable"
+// 	serverAddress = "localhost:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatalln("Cannot load config files", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to database", err)
@@ -25,7 +32,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start sever ", err)
