fb90c5f235845b9fb37f1c8de81ac17cbb6d3c32
dutt23 <shatyaki_dutt@hotmail.com>
2022-10-28 00:03:24 +0530

    Add create user tests

--- api/main_test.go
+++ api/main_test.go
@@ -12,3 +12,15 @@ func TestMain(m *testing.M) {
 	gin.SetMode(gin.DebugMode)
 	os.Exit(m.Run())
 }
+
+// func newTestServer(t *testing.T, store db.Store) *Server {
+// 	config := util.Config{
+// 		TokenSymmetricKey:   util.RandomString(32),
+// 		AccessTokenDuration: time.Minute,
+// 	}
+
+// 	server, err := NewServer(config, store)
+// 	require.NoError(t, err)
+
+// 	return server
+// }
